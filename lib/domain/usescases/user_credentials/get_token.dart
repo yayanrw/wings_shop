5738129934da720f59e8894bf65bc32467972da1
yayanrw <yayanraw@gmail.com>
2023-06-25 00:07:58 +0700

    add get_token.dart

---
+++ lib/domain/usescases/user_credentials/get_token.dart
@@ -0,0 +1,22 @@
+import 'package:wings_shop/domain/repositories/user_credentials_repository.dart';
+
+class GetToken {
+  final UserCredentialsRepository userCredentialsRepository;
+
+  GetToken(this.userCredentialsRepository);
+
+  Future<String?> exec() async {
+    final response = await userCredentialsRepository.getUserCredentials();
+
+    final userCredentials = response.fold(
+      (failure) => null,
+      (data) => data,
+    );
+
+    if (userCredentials != null) {
+      return userCredentials.token;
+    } else {
+      return null;
+    }
+  }
+}
