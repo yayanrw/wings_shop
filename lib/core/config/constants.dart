68c15684e02cd1c31a0a057d1435fb30fae6cf2a
yayanrw <yayanraw@gmail.com>
2023-06-24 14:11:33 +0700

    add constants.dart

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -3,4 +3,4 @@ const String fontFamily = 'Lato';
 const double defaultRadius = 32;
 const double secondRadius = 22;
 const double defaultPadding = 26;
-const String baseUrl = 'http://127.0.0.1:8000/api/';
+const String baseUrl = 'http://127.0.0.1:8000/api';
