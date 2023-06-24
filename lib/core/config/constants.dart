0b520f6681c110cad7c0b23d0556e3ba23eb558f
yayanrw <yayanraw@gmail.com>
2023-06-24 18:19:24 +0700

    update

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -2,6 +2,6 @@ const String appTitle = 'Wings Shop';
 const String fontFamily = 'Lato';
 const double defaultRadius = 32;
 const double secondRadius = 22;
-const double defaultPadding = 26;
+const double defaultPadding = 16;
 const String baseUrl = 'http://127.0.0.1:8000/api';
 const String userCredentialsKey = 'user_credentials';
