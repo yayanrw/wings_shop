e4faf7b15fb9ea713ee7d593820de8fe4a5da11a
yayanrw <yayanraw@gmail.com>
2023-06-24 17:04:48 +0700

    init lgin

--- lib/presentation/pages/login/login_page.dart
+++ lib/presentation/pages/login/login_page.dart
@@ -7,6 +7,10 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: Center(
+        child: Text("Hello World"),
+      ),
+    );
   }
 }
