f2964f249d12c24f43fefceac1b339e3dbff4759
yayanrw <yayanraw@gmail.com>
2023-06-24 15:13:10 +0700

    add user_credentials_data_source.dart

---
+++ lib/data/datasources/local/user_credentials_data_source.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+
+import 'package:injectable/injectable.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wings_shop/core/config/constants.dart';
+import 'package:wings_shop/data/datasources/local/daos/user_credentials_dao.dart';
+
+abstract class UserCredentialsDataSource {
+  Future<UserCredentialsDao> getUserCredentials();
+
+  Future<bool> setUserCredentials(UserCredentialsDao userCredentialsDao);
+
+  Future<bool> clearUserCredentials();
+}
+
+@LazySingleton(as: UserCredentialsDataSource)
+class UserCredentialsDataSourceImpl implements UserCredentialsDataSource {
+  final sharedPref = SharedPreferences.getInstance();
+
+  @override
+  Future<bool> clearUserCredentials() async {
+    final prefs = await sharedPref;
+    return prefs.clear();
+  }
+
+  @override
+  Future<UserCredentialsDao> getUserCredentials() async {
+    final prefs = await sharedPref;
+    final userCredentials = prefs.getString(userCredentialsKey);
+
+    if (userCredentials == null) {
+      return UserCredentialsDao();
+    } else {
+      return UserCredentialsDao.fromJson(
+        jsonDecode(userCredentials),
+      );
+    }
+  }
+
+  @override
+  Future<bool> setUserCredentials(UserCredentialsDao userCredentialsDao) async {
+    final prefs = await sharedPref;
+    final jsonString = jsonEncode(userCredentialsDao);
+    prefs.setString(userCredentialsKey, jsonString);
+    return true;
+  }
+}
