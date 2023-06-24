2dc0eda556319a8b7b99063f060afe68a22b2ebe
yayanrw <yayanraw@gmail.com>
2023-06-24 17:02:13 +0700

    init router

---
+++ lib/core/router/router.dart
@@ -0,0 +1,28 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:wings_shop/presentation/pages/cart/cart_page.dart';
+import 'package:wings_shop/presentation/pages/detail/detail_page.dart';
+import 'package:wings_shop/presentation/pages/home/home_page.dart';
+import 'package:wings_shop/presentation/pages/login/login_page.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(
+      page: LoginRoute.page,
+      initial: true,
+    ),
+    AutoRoute(
+      page: HomeRoute.page,
+    ),
+    AutoRoute(
+      page: CartRoute.page,
+    ),
+    AutoRoute(
+      path: 'detail/:id',
+      page: DetailRoute.page,
+    ),
+  ];
+}
