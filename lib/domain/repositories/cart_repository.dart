180f609411c1d0c54795e0eaf6bed3d97954679b
yayanrw <yayanraw@gmail.com>
2023-06-24 16:30:57 +0700

    update cart_repository.dart

--- lib/domain/repositories/cart_repository.dart
+++ lib/domain/repositories/cart_repository.dart
@@ -1,11 +1,12 @@
 import 'package:dartz/dartz.dart';
 import 'package:wings_shop/core/utils/errors/failure.dart';
+import 'package:wings_shop/data/datasources/remote/params/cart_params.dart';
 import 'package:wings_shop/domain/entities/carts/cart.dart';
 
 abstract class CartRepository {
   Future<Either<Failure, List<Cart>>> getCarts();
 
-  Future<Either<Failure, bool>> addCart(Cart cart);
+  Future<Either<Failure, bool>> addCart(CartParams cartParams);
 
   Future<Either<Failure, bool>> updateCart({
     required int id,
