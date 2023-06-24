319850e5b29e177bc777f7ffbf77c6140a555c16
yayanrw <yayanraw@gmail.com>
2023-06-24 20:55:20 +0700

    fix login_notifier.dart

--- lib/presentation/pages/login/login_notifier.dart
+++ lib/presentation/pages/login/login_notifier.dart
@@ -1,5 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
+import 'package:wings_shop/core/utils/request_state.dart';
+import 'package:wings_shop/domain/usescases/auth/login.dart';
 
 @injectable
-class LoginNotifier extends ChangeNotifier {}
+class LoginNotifier extends ChangeNotifier {
+  final LogIn logIn;
+
+  LoginNotifier(this.logIn);
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  RequestState _requestState = RequestState.empty;
+  String _message = '';
+
+  RequestState get requestState => _requestState;
+
+  String get message => _message;
+
+  Future<void> fetchLogin() async {
+    try {
+      _requestState = RequestState.loading;
+      notifyListeners();
+
+      final result = await logIn.exec(
+        email: emailController.text,
+        password: passwordController.text,
+      );
+
+      result.fold(
+        (failure) {
+          _requestState = RequestState.error;
+          _message = failure.message;
+          notifyListeners();
+        },
+        (_) {
+          _requestState = RequestState.loaded;
+          notifyListeners();
+        },
+      );
+    } catch (e) {
+      _requestState = RequestState.error;
+      _message = 'An error occurred: ${e.toString()}';
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+}
