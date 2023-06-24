1eee99ef48fdc5ff460cc930185903b768f6ae3d
yayanrw <yayanraw@gmail.com>
2023-06-24 20:23:41 +0700

    update baseUrl

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -3,5 +3,5 @@ const String fontFamily = 'Lato';
 const double defaultRadius = 32;
 const double secondRadius = 22;
 const double defaultPadding = 16;
-const String baseUrl = 'http://127.0.0.1:8000/api';
+const String baseUrl = 'http://192.168.1.7:87/api';
 const String userCredentialsKey = 'user_credentials';
