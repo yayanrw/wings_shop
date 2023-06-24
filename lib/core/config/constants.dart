9b6739aca6aacb73eefaac85b3871622e23547c8
yayanrw <yayanraw@gmail.com>
2023-06-24 15:13:18 +0700

    add userCredKey

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -4,3 +4,4 @@ const double defaultRadius = 32;
 const double secondRadius = 22;
 const double defaultPadding = 26;
 const String baseUrl = 'http://127.0.0.1:8000/api';
+const String userCredentialsKey = 'user_credentials';
