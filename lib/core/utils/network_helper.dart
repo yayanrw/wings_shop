eb066a3f0f836c386af2397120b39e1c260cd6fd
yayanrw <yayanraw@gmail.com>
2023-06-25 01:49:18 +0700

    fix network_helper.dart

--- lib/core/utils/network_helper.dart
+++ lib/core/utils/network_helper.dart
@@ -6,39 +6,43 @@ import 'package:http/http.dart' as http;
 import 'package:wings_shop/core/utils/errors/exceptions.dart';
 import 'package:wings_shop/core/utils/errors/failure.dart';
 
-Map<String, String> defaultHeader() {
-  return {
+class NetworkHelper {
+  static Map<String, String> defaultHeader = {
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded',
   };
-}
-
-Map<String, String> headerWithToken() {
-  return {
-    'Authorization': 'Bearer ',
+  static Map<String, String> headerWithToken = {
     'Accept': 'application/json',
     'Content-Type': 'application/x-www-form-urlencoded',
   };
-}
 
-void throwExceptionIfClientError(http.Response response) {
-  if (response.statusCode >= 400 && response.statusCode <= 499) {
-    final errorResponse = jsonDecode(response.body);
-    final message = errorResponse['message'];
-    throw ApplicationException(message);
+  static Map<String, String> headerWithTokenz(String? token) {
+    return {...defaultHeader, 'Authorization': 'Bearer $token'};
   }
-}
 
-Future<Either<Failure, T>> executeSafely<T>(
-    Future<T> Function() action) async {
-  try {
-    final result = await action();
-    return Right(result);
-  } on ApplicationException catch (e) {
-    return Left(ApplicationFailure(e.message));
-  } on ServerException catch (e) {
-    return Left(ServerFailure(e.toString()));
-  } on SocketException {
-    return const Left(ConnectionFailure("Connection failed"));
+  static void setHeaderWithToken(String token) {
+    headerWithToken = {...defaultHeader, 'Authorization': 'Bearer $token'};
+  }
+
+  static void throwExceptionIfClientError(http.Response response) {
+    if (response.statusCode >= 400 && response.statusCode <= 499) {
+      final errorResponse = jsonDecode(response.body);
+      final message = errorResponse['message'];
+      throw ApplicationException(message);
+    }
+  }
+
+  static Future<Either<Failure, T>> executeSafely<T>(
+      Future<T> Function() action) async {
+    try {
+      final result = await action();
+      return Right(result);
+    } on ApplicationException catch (e) {
+      return Left(ApplicationFailure(e.message));
+    } on ServerException catch (e) {
+      return Left(ServerFailure(e.toString()));
+    } on SocketException {
+      return const Left(ConnectionFailure("Connection failed"));
+    }
   }
 }
