b5d0e8bbb7b9b161a0b6089a7973a3a6757b2a59
yayanrw <yayanraw@gmail.com>
2023-06-24 16:49:28 +0700

    update login

--- lib/domain/usescases/auth/login.dart
+++ lib/domain/usescases/auth/login.dart
@@ -1,15 +1,17 @@
 import 'package:dartz/dartz.dart';
+import 'package:injectable/injectable.dart';
 import 'package:wings_shop/core/utils/errors/failure.dart';
 import 'package:wings_shop/domain/entities/auth/login_data.dart';
 import 'package:wings_shop/domain/entities/user_credentials/user_credentials.dart';
 import 'package:wings_shop/domain/repositories/auth_repository.dart';
 import 'package:wings_shop/domain/repositories/user_credentials_repository.dart';
 
-class logIn {
+@lazySingleton
+class LogIn {
   final AuthRepository authRepository;
   final UserCredentialsRepository userCredentialsRepository;
 
-  logIn(this.authRepository, this.userCredentialsRepository);
+  LogIn(this.authRepository, this.userCredentialsRepository);
 
   Future<Either<Failure, LoginData>> exec({
     required String email,
