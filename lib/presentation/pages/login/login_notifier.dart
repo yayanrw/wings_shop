d70674098e585fd5859f78c37961daec16733e39
yayanrw <yayanraw@gmail.com>
2023-06-24 21:44:17 +0700

    isloggedin

--- lib/presentation/pages/login/login_notifier.dart
+++ lib/presentation/pages/login/login_notifier.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
 import 'package:wings_shop/core/utils/request_state.dart';
+import 'package:wings_shop/domain/entities/user_credentials/user_credentials.dart';
+import 'package:wings_shop/domain/repositories/user_credentials_repository.dart';
 import 'package:wings_shop/domain/usescases/auth/login.dart';
 
 @injectable
 class LoginNotifier extends ChangeNotifier {
   final LogIn logIn;
+  final UserCredentialsRepository userCredentialsRepository;
 
-  LoginNotifier(this.logIn);
+  LoginNotifier(this.logIn, this.userCredentialsRepository);
 
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -18,6 +21,29 @@ class LoginNotifier extends ChangeNotifier {
   RequestState get requestState => _requestState;
 
   String get message => _message;
+
+  Future<bool> isLoggedIn() async {
+    try {
+      final result = await userCredentialsRepository.getUserCredentials();
+      final resultFold = result.fold(
+        (failure) {
+          _message = failure.message;
+          notifyListeners();
+          return false;
+        },
+        (UserCredentials userCredentials) {
+          notifyListeners();
+          return userCredentials.token.isNotEmpty &&
+              userCredentials.token != "N/A";
+        },
+      );
+      return resultFold;
+    } catch (e) {
+      _message = 'An error occurred: ${e.toString()}';
+      notifyListeners();
+      return false;
+    }
+  }
 
   Future<void> fetchLogin() async {
     try {
