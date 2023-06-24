b399c8245c50c208c7c5415605e8b60c1ab81eed
yayanrw <yayanraw@gmail.com>
2023-06-24 19:34:36 +0700

    update exceptions.dart

--- lib/core/utils/errors/exceptions.dart
+++ lib/core/utils/errors/exceptions.dart
@@ -1,7 +1,17 @@
-class ServerException implements Exception {}
+class ServerException implements Exception {
+  final String message;
+
+  ServerException(this.message);
+}
 
 class DatabaseException implements Exception {
   DatabaseException(this.message);
 
   final String message;
+}
+
+class ApplicationException implements Exception {
+  final String message;
+
+  ApplicationException(this.message);
 }
