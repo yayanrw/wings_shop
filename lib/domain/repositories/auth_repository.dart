42717a996540e0cb65567bb086bca19e5465169f
yayanrw <yayanraw@gmail.com>
2023-06-24 16:00:21 +0700

    update login

--- lib/domain/repositories/auth_repository.dart
+++ lib/domain/repositories/auth_repository.dart
@@ -3,6 +3,10 @@ import 'package:wings_shop/core/utils/errors/failure.dart';
 import 'package:wings_shop/domain/entities/auth/login_data.dart';
 
 abstract class AuthRepository {
-  Future<Either<Failure, LoginData>> logIn();
+  Future<Either<Failure, LoginData>> logIn({
+    required String email,
+    required String password,
+  });
+
   Future<Either<Failure, bool>> logOut();
 }
