b04f61f47b010db270b514d5f55bc3af80e2ab76
yayanrw <yayanraw@gmail.com>
2023-06-24 15:40:01 +0700

    add user_credentials.dart

---
+++ lib/domain/entities/user_credentials/user_credentials.dart
@@ -0,0 +1,35 @@
+import 'package:equatable/equatable.dart';
+import 'package:wings_shop/data/datasources/local/daos/user_credentials_dao.dart';
+
+class UserCredentials extends Equatable {
+  final int id;
+  final String name;
+  final String email;
+  final String token;
+
+  const UserCredentials({
+    required this.id,
+    required this.name,
+    required this.email,
+    required this.token,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        name,
+        email,
+        token,
+      ];
+}
+
+extension UserCredentialsDaoExt on UserCredentialsDao {
+  UserCredentials toEntity() {
+    return UserCredentials(
+      id: id ?? 0,
+      name: name ?? "N/A",
+      email: email ?? "N/A",
+      token: token ?? "N/A",
+    );
+  }
+}
