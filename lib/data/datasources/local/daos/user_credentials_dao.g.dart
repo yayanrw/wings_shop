ea0af497c011371eaffe573b46941542b62847c2
yayanrw <yayanraw@gmail.com>
2023-06-24 16:32:51 +0700

    add

--- lib/data/datasources/local/daos/user_credentials_dao.g.dart
+++ lib/data/datasources/local/daos/user_credentials_dao.g.dart
@@ -8,10 +8,10 @@ part of 'user_credentials_dao.dart';
 
 UserCredentialsDao _$UserCredentialsDaoFromJson(Map<String, dynamic> json) =>
     UserCredentialsDao(
-      id: json['id'] as int,
-      name: json['name'] as String,
-      email: json['email'] as String,
-      token: json['token'] as String,
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+      email: json['email'] as String?,
+      token: json['token'] as String?,
     );
 
 Map<String, dynamic> _$UserCredentialsDaoToJson(UserCredentialsDao instance) =>
